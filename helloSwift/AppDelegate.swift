22406c21bcb8ef7c668798c90775535b478d0afa
ogaksk <ogawa@ogawa-air.local>
2014-09-05 18:35:25 +0900

    Initial Commit

---
+++ helloSwift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  helloSwift
+//
+//  Created by ogawa on 2014/09/05.
+//  Copyright (c) 2014年 ogawa. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification?) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification?) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ helloSwift/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  helloSwift
+//
+//  Created by ogawa on 2014/09/05.
+//  Copyright (c) 2014年 ogawa. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
